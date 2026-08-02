c39da4f932c440300e25f00450613e42f37df535
jesusl392 <jesusslozano@ucundinamarca.edu.co>
2026-08-02 16:40:10 -0500

    arreglo zona horaria

--- src/main/java/com/example/recordatorios_esp32/controller/RecordatorioController.java
+++ src/main/java/com/example/recordatorios_esp32/controller/RecordatorioController.java
@@ -7,19 +7,22 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.List;
 
 @RestController
 @RequestMapping("/recordatorios")
 public class RecordatorioController {
 
+    private static final ZoneId ZONA_COLOMBIA = ZoneId.of("America/Bogota");
+
     @Autowired
     private RecordatorioRepository repo;
 
     @GetMapping("/pendientes")
     public List<Recordatorio> pendientes() {
-        LocalDate hoy = LocalDate.now();
-        LocalTime horaActual = LocalTime.now();
+        LocalDate hoy = LocalDate.now(ZONA_COLOMBIA);
+        LocalTime horaActual = LocalTime.now(ZONA_COLOMBIA);
         return repo.findVencidos(hoy, horaActual);
     }
 
