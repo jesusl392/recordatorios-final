3ae6bc32e8f6427b4b75af3900bce5fcd504ce23
jesusl392 <jesusslozano@ucundinamarca.edu.co>
2026-08-02 18:27:10 -0500

    recordatorios de hoy

--- src/main/java/com/example/recordatorios_esp32/model/Recordatorio.java
+++ src/main/java/com/example/recordatorios_esp32/model/Recordatorio.java
@@ -16,7 +16,6 @@ public class Recordatorio {
     private boolean activo = true;
     private boolean mostrado = false;
 
-    // Getters y setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getTitulo() { return titulo; }
